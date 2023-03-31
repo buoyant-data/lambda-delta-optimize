45ec8d5d5d3041071b1bb2208a1d00891523035a
R. Tyler Croy <rtyler@brokenco.de>
2023-03-30 23:04:52 -0700

    Introduce the OPTIMIZE_DS environment variable to scope optimize
    The only supported value at the moment is "yesterday" which will only optimize
    the `ds` partition for the prior day UTC.

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,41 @@ async fn main() -> Result<(), Error> {
     lambda_runtime::run(func).await
 }
 
+const OPTIMIZE_DS_YESTERDAY: &str = "yesterday";
+
 /*
  * Lambda function handler
  */
-async fn func(event: LambdaEvent<CloudWatchEvent>) -> Result<Value, Error> {
+async fn func<'a>(event: LambdaEvent<CloudWatchEvent>) -> Result<Value, Error> {
+    use deltalake::PartitionFilter;
+    use chrono::*;
+
     debug!("CloudWatch event: {:?}", event);
     let location = std::env::var("DATALAKE_LOCATION")?;
     let table = deltalake::open_table(&location).await?;
-    let (table, metrics) = OptimizeBuilder::new(table.object_store(), table.state).await?;
+
+    let mut filters = vec![];
+    // This variable only exists to provide a long enough lifetime for
+    // any PartitionFilter values created
+    let mut _ds = String::new();
+
+    if let Ok(ds_filter) = std::env::var("OPTIMIZE_DS") {
+        match ds_filter.as_str() {
+            OPTIMIZE_DS_YESTERDAY => {
+                if let Some(yesterday) = Utc::now().checked_sub_days(Days::new(1)) {
+                    _ds = format!("{}", yesterday.format("%Y-%m-%d"));
+                    let partition = ("ds", "=", _ds.as_str());
+                    info!("Optimizing with partition: {:?}", partition);
+                    filters.push(PartitionFilter::try_from(partition)?);
+                }
+            },
+            unknown => warn!("Unknown value of OPTIMIZE_DS: {}", unknown),
+        }
+    }
+
+    let (table, metrics) = OptimizeBuilder::new(table.object_store(), table.state)
+        .with_filters(filters.as_slice())
+        .await?;
 
     debug!("table: optimize: {:?}", table);
     info!("table: metrics: {:?}", metrics);
